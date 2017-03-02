c48e33dca5d89a1d7e236a9caed61c70bf2d9afa
yanghao0518 <1017607034@qq.com>
2017-03-03 01:51:16 +0800

    修改页面逻辑

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -36,3 +36,135 @@ baseRequest.prototype.send = function(data) {
 		}
 	});
 }
+
+var baseCheckbox = function baseCheckbox(ctype,byteSize){
+	this.ctype = ctype;
+	this.byteSize = byteSize;
+}
+
+baseCheckbox.prototype.buzero = function(str,size){
+	if(size > 0){
+		for(var j=0;j<size;j++){
+			str + = '0';
+		}
+	}
+	return str;
+}
+
+baseCheckbox.prototype.getValue = function(){
+	var types = $('input[ctype="'+this.ctype+'"]');
+	var str = ''; 
+	var i = 1;
+	$.each(types,function(index,demo){
+		if(demo.checked){
+			str = str + '1';
+		}else{
+			str = str + '0';
+		}
+		i++;
+	});
+	str = this.buzero(str,(this.byteSize - i));
+	return str;
+};
+
+baseCheckbox.prototype.getValueBy10 = function(){
+	var str = this.getValue(); 
+	return parseInt(str,2);
+};
+
+baseCheckbox.prototype.setValueBy10 = function(value){
+	value =  value.toString(2);
+	var types = $('input[ctype="'+this.ctype+'"]');
+	$.each(types,function(index,demo){
+		var x = value.charAt(index);
+		if(x == '0'){
+			demo.checked = false;
+		}else{
+			demo.checked = true;
+		}
+		
+	});
+};
+
+baseCheckbox.prototype.setValue = function(value){
+	var types = $('input[ctype="'+this.ctype+'"]');
+	$.each(types,function(index,demo){
+		var x = value.charAt(index);
+		if(x == '0'){
+			demo.checked = false;
+		}else{
+			demo.checked = true;
+		}
+		
+	});
+};
+
+var baseForm = function baseForm(id){
+	this.id = id;
+	this.$form = $('#'+id);
+}
+
+baseForm.prototype.getSendData = function(){
+	var array = this.$form.serializeArray();
+	var obj = {};
+	for (var arr in array) {
+		var objName = array[arr]['name'];
+		var objValue = array[arr]['value'];
+		if(!objValue || objValue.length < 1) continue;
+		if(obj[objName] != undefined){
+			obj[objName] = obj[objName] + ',' + objValue;
+		}else{
+			obj[objName] = objValue;
+		}
+	}
+	return obj;
+};
+baseForm.prototype.save = function(url,call){
+	var request = new baseRequest(url,call);
+	var formdata = this.getSendData();
+	request.send({'formdata':JSON.stringify(formdata)});
+};
+baseForm.prototype.load = function(url,beforLoad){
+	var request = new baseRequest(url,function(result){
+		if(result && result.code == 200 && result.data){
+			this.loadData(result.data);
+			if(beforLoad){
+				beforLoad(result.data);
+			}
+		}else{
+			layer.msg('加载数据失败!');
+		}
+	});
+	request.send({});
+};
+
+baseForm.prototype.loadData = function(obj){
+	var key,value,tagName,type,arr;
+	for(x in obj){
+		key = x;
+		value = obj[x];
+		
+		$("[name='"+key+"'],[name='"+key+"[]']").each(function(){
+			tagName = $(this)[0].tagName;
+			type = $(this).attr('type');
+			if(tagName=='INPUT'){
+				if(type=='radio'){
+					$(this).attr('checked',$(this).val()==value);
+				}else if(type=='checkbox'){
+					arr = value.split(',');
+					for(var i =0;i<arr.length;i++){
+						if($(this).val()==arr[i]){
+							$(this).attr('checked',true);
+							break;
+						}
+					}
+				}else{
+					$(this).val(value);
+				}
+			}else if(tagName=='SELECT' || tagName=='TEXTAREA'){
+				$(this).val(value);
+			}
+			
+		});
+	}
+}
